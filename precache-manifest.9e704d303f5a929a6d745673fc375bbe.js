93d138819c258ab01f97d9dc42db36f886df929e
devgony <devgony@gmail.com>
2020-10-05 01:24:55 +0900

    Updates

---
+++ precache-manifest.9e704d303f5a929a6d745673fc375bbe.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5953f38e738472a66739ca994fd551a5",
+    "url": "/cherish/index.html"
+  },
+  {
+    "revision": "d3b54726be3301d0f45b",
+    "url": "/cherish/static/css/main.950c2d59.chunk.css"
+  },
+  {
+    "revision": "3ec7d1d92815ab3620b4",
+    "url": "/cherish/static/js/2.d5971b31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cherish/static/js/2.d5971b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b54726be3301d0f45b",
+    "url": "/cherish/static/js/main.f457fc20.chunk.js"
+  },
+  {
+    "revision": "ab3c2510238e12bef14a",
+    "url": "/cherish/static/js/runtime-main.8c9fe7c6.js"
+  },
+  {
+    "revision": "2bd4cbe5f299d1b1084b2724ce6835e2",
+    "url": "/cherish/static/media/GmarketSansBold.2bd4cbe5.woff2"
+  },
+  {
+    "revision": "9182ea25ab8a70f98a3f4c27bf489c05",
+    "url": "/cherish/static/media/GmarketSansBold.9182ea25.woff"
+  },
+  {
+    "revision": "4a325945e49a6ffe45a0ab893d6246a4",
+    "url": "/cherish/static/media/GmarketSansMedium.4a325945.woff"
+  },
+  {
+    "revision": "a360f30a8f1cbbd9359585d55eb440e0",
+    "url": "/cherish/static/media/GmarketSansMedium.a360f30a.woff2"
+  },
+  {
+    "revision": "9dbfecc571cd336ad07f3e9a4d863546",
+    "url": "/cherish/static/media/img_1.9dbfecc5.png"
+  },
+  {
+    "revision": "b7007f4011b991ee363569bb0082ed90",
+    "url": "/cherish/static/media/img_10.b7007f40.png"
+  },
+  {
+    "revision": "892887a1f0df195b491881f80bde993c",
+    "url": "/cherish/static/media/img_11.892887a1.png"
+  },
+  {
+    "revision": "892887a1f0df195b491881f80bde993c",
+    "url": "/cherish/static/media/img_12.892887a1.png"
+  },
+  {
+    "revision": "a4119dcf7c7a646fd2264c7185b90638",
+    "url": "/cherish/static/media/img_13.a4119dcf.png"
+  },
+  {
+    "revision": "aa501110b49eb74836ea465682bd1715",
+    "url": "/cherish/static/media/img_14.aa501110.png"
+  },
+  {
+    "revision": "42c73541d98b51a13fcccb5dfdef256c",
+    "url": "/cherish/static/media/img_2.42c73541.png"
+  },
+  {
+    "revision": "6c56cbddca3a6d4a535de56a2d56debd",
+    "url": "/cherish/static/media/img_3.6c56cbdd.png"
+  },
+  {
+    "revision": "568fbefc946de800c2a63ecac59167eb",
+    "url": "/cherish/static/media/img_4.568fbefc.png"
+  },
+  {
+    "revision": "8a55e32810d44ac310c04dedde3af066",
+    "url": "/cherish/static/media/img_5.8a55e328.png"
+  },
+  {
+    "revision": "54fc762f89285e12c8e754a14a836143",
+    "url": "/cherish/static/media/img_6.54fc762f.png"
+  },
+  {
+    "revision": "b922b214f5080482482810fb077fa29f",
+    "url": "/cherish/static/media/img_7.b922b214.png"
+  },
+  {
+    "revision": "30a60c4382bf85c3980950e475bd22c9",
+    "url": "/cherish/static/media/img_8.30a60c43.png"
+  },
+  {
+    "revision": "e7163f54d617e3bfe4e2254ff9d946a6",
+    "url": "/cherish/static/media/img_9.e7163f54.png"
+  },
+  {
+    "revision": "cd41c4ff05f5347d562a1c183afcca4a",
+    "url": "/cherish/static/media/logo.cd41c4ff.png"
+  }
+]);
